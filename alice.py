73ef8a5a8fcb749df2c53fe810c7a7cea7276d37
happice <14.alice.lee@gmail.com>
2025-02-02 02:49:13 +0000

    20250202

---
+++ alice.py
@@ -0,0 +1,13 @@
+# To run, copy and paste the code below.
+# streamlit run alice.py --server.port 8080 --server.address 0.0.0.0
+
+# streamlit official doc link:
+# https://docs.streamlit.io/
+import streamlit as st
+
+st.title("The Last Rebellion")
+st.write("Apple, Spagetti, Bubble Tea, Donut, Gummy Bear")
+# background-color: #a8d5ba;
+#background-size: cover;
+#background: url(...)
+
